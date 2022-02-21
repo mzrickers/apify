3cf60ef99529ced4ec99e47d64fe4f1e85d57cbf
Zach Rickers <mzrickers@gmail.com>
2022-02-21 12:31:53 +0100

    added algorithm and gitignore

---
+++ index.js
@@ -0,0 +1,35 @@
+const apiUrl = "https://api.ecommerce.com/products";
+let productArray = [];
+let lowPrice = 0;
+let highPrice = 100000;
+
+const countProduct = async (lowPrice, highPrice) => {
+  const response = await axios.get(apiUrl, {
+    params: {
+      minPrice: lowPrice,
+      maxPrice: highPrice
+    }
+  });
+
+  let lowerLimit = lowPrice;
+  let upperLimit = highPrice;
+
+  while (upperLimit > lowerLimit) {
+    // if there is greater than 1000 you will miss out on counting some products
+    if (response.data.count >= 1000) {
+      // call function recursively by splitting search area in half
+        countProduct(lowerLimit, (lowerLimit + upperLimit / 2));
+    } else {
+      response.data.products.forEach(product => {
+        if (product.price > lowerLimit) {  // update the price so you have new minimum at end 
+          lowerLimit = product.price;
+        } 
+        // push each product to the array
+        productArray.push(product);
+      })
+      // increment lower limit and upper limit back to 100000 so you have a new range of prices
+      lowerLimit += .01;
+      upperLimit = 100000;
+    };
+  }
+};
